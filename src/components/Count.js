39adb146154ef239949ebeabded6af4a502b4195
siddharth-69 <duttasidb.1998@gmail.com>
2020-07-24 19:01:06 +0530

    Update Count.js

--- src/components/Count.js
+++ src/components/Count.js
@@ -9,8 +9,16 @@ class Count extends Component {
 				<h1>COUNT: {this.props.ctr}</h1>
 				<Button handleClick={this.props.onChange1} children="INC" />
 				<Button handleClick={this.props.onChange2} children="DEC" />
-				<Button handleClick={this.props.onChange3} children="ADD5"/>
-				<Button handleClick={this.props.onChange4} children="SUB5"/>
+				<Button handleClick={this.props.onChange3} children="ADD10"/>
+				<Button handleClick={this.props.onChange4} children="SUB10"/>
+				<hr />
+				<Button handleClick={this.props.onChange5} children="STORE" />
+				<ul>
+					{this.props.STR.map((x)=> (
+						<li onClick={()=>this.props.onChange6(x.id)}>{x.val}</li>
+					))}
+				
+				</ul>
 			</div>
 		)
 	}
@@ -18,15 +26,18 @@ class Count extends Component {
 
 const mapStateToProps=(state)=>{
 	return {
-		ctr: state.Counter
+		ctr: state.Counter,
+		STR: state.Results
 	}
 }
 const mapDispatchToProps=(dis)=>{
 	return {
 		onChange1: ()=> dis({type:"inc"}),
 		onChange2: ()=> dis({type:"dec"}),
-		onChange3: ()=> dis({type:"add5"}),
-		onChange4: ()=> dis({type:"sub5"})
+		onChange3: ()=> dis({type:"add10",val:10}),
+		onChange4: ()=> dis({type:"sub10",val:10}),
+		onChange5: ()=> dis({type:"STORE"}),
+		onChange6: (id)=> dis({type:"DELE",RID:id}),
 	}
 }
 export default connect(mapStateToProps,mapDispatchToProps)(Count)
