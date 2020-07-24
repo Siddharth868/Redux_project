d0bc8961a809951af9b6489a67a88c870e2741b3
siddharth-69 <duttasidb.1998@gmail.com>
2020-07-24 19:01:47 +0530

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,26 @@
+import * as actionTypes from './actions'
+
 const ini={
-    Counter:0
+    Counter:0,
+    Results:[]
 }
 
 
 const reducer=(state=ini,action)=>{
-    if(action.type==="inc"){
-        return {Counter:state.Counter+1}
-    }
-    if(action.type==="dec"){
-        return {Counter:state.Counter-1}
-    }
-    if(action.type==="add5"){
-        return {Counter:state.Counter+5}
-    }
-    if(action.type==="sub5"){
-        return {Counter:state.Counter-5}
+    switch(action.type){
+        case actionTypes.inc:
+            return {...state,Counter:state.Counter+1}
+        case actionTypes.dec:
+            return {...state,Counter:state.Counter-1}
+        case actionTypes.add10:
+            return {...state,Counter:state.Counter+action.val} 
+        case actionTypes.sub10:
+            return {...state,Counter:state.Counter-action.val} 
+        case actionTypes.STORE:
+            return {...state,Results:state.Results.concat({id:new Date(),val:state.Counter})}
+        case actionTypes.DELE:
+            const new_array=state.Results.filter((x)=>x.id!==action.RID)
+            return {...state,Results:new_array}
     }
     return state
 }
