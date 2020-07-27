bb958a5cce8eab526113502a155ae174315aaddd
siddharth-69 <duttasidb.1998@gmail.com>
2020-07-27 21:40:16 +0530

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -4,3 +4,47 @@ export const add10 = "add10"
 export const sub10 = "sub10"
 export const STORE = "STORE"
 export const DELE = "DELE"
+
+
+export const incrementAsync=()=>{
+    return {
+        type: inc
+    }
+}
+
+export const increment=()=>{
+    return (dispatch)=>{
+        setTimeout(()=>{
+            dispatch(incrementAsync())
+        },4000)
+    }
+}
+
+export const decrement=()=>{
+    return {
+        type: dec
+    }
+}
+export const add=(value)=>{
+    return {
+        type: add10,
+        val: value
+    }
+}
+export const sub=(value)=>{
+    return {
+        type: sub10,
+        val: value
+    }
+}
+export const store1=()=>{
+    return {
+        type: STORE
+    }
+}
+export const delete1=(id)=>{
+    return {
+        type: DELE,
+        RID: id
+    }
+}
